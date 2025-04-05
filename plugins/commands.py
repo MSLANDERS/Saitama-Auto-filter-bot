0a3abe8942eba1cd7cd23788a1bce7918851df0c
MSLANDERS <mslanders0@gmail.com>
2025-04-05 21:11:30 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -346,7 +346,7 @@ async def start(client:Client, message):
                 file_caption=file.caption
             )
             btn = [[
-                InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f'stream#{file.file_id}')
+                InlineKeyboardButton("🖥 WATCH & DOWNLOAD 🚀", url="https://t.me/FilesToLinkPro_bot")
             ]]
             toDel = await client.send_cached_media(
                 chat_id=message.from_user.id,
@@ -386,7 +386,7 @@ async def start(client:Client, message):
         file_caption=files.caption
     )
     btn = [[
-        InlineKeyboardButton("✛ ᴡᴀᴛᴄʜ & ᴅᴏᴡɴʟᴏᴀᴅ ✛", callback_data=f'stream#{file_id}')
+        InlineKeyboardButton("🖥 WATCH & DOWNLOAD 🚀", url="https://t.me/FilesToLinkPro_bot")
     ]]
     toDel=await client.send_cached_media(
         chat_id=message.from_user.id,
